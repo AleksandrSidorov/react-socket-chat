c70c01fbe9881a9c91387a66c243e78c882c96b9
Aleksandr Sidorov <shitl80@gmail.com>
2017-02-24 20:46:05 +0700

    Username field added

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      inputValue: '',
+      name: null,
       users: [],
       messages: [],
       socket: io('http://localhost:3030')
@@ -13,13 +15,63 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.state.socket.emit('new-message', 'assa')
+    this.state.socket.on('recieve-message', msg => {
+      var messages = this.state.messages
+      messages.push(msg)
+      this.setState({ messages })
+    })
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value })
+  }
+
+  handleNameSubmit = () => {
+    const name = this.nameInput.value
+    console.log(name)
+    this.setState({ name })
+  }
+
+  handleMessageSubmit = (event) => {
+    event.preventDefault()
+    const msg = this.state.inputValue
+    const name = this.state.name
+    this.state.socket.emit('new-message', msg)
+    this.setState({ inputValue: '' })
   }
 
   render() {
+    const { messages, name } = this.state
+    let nameBlock = null
+    if (!name) {
+      nameBlock = (
+        <div>
+          <input type="text" placeholder="anonymous" ref={(input) => { this.nameInput = input}} />
+          <button type="submit" onClick={this.handleNameSubmit}>Set Name</button>
+        </div>
+      )
+    } else {
+      nameBlock = (
+        <div>
+          <strong>{name}</strong>
+        </div>
+      )
+    }
     return (
       <div>
         <h1>React + Socket.io Chat App</h1>
+        <div>
+          <ul>
+            {messages.map((message, index) => {
+              return <li key={index}>{message}</li>
+            })}
+          </ul>
+        </div>
+        {nameBlock}
+        <div>
+          <input type="text" value={this.state.inputValue} onChange={this.handleInputChange} />
+          <button type="submit" onClick={this.handleMessageSubmit}>Send</button>
+        </div>
       </div>
     )
   }
