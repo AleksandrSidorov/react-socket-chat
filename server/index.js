d1bae1e79618e67341deba6bf60b5b5deec59f40
Aleksandr Sidorov <shtil80@gmail.com>
2017-02-27 09:15:38 +0700

    Port settings for heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server)
+
+app.use (express.static(path.resolve(__dirname, '../ui/build')))
+
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../ui/build', 'index.html'))
+})
 
 io.on('connection', function(socket) {
   console.log("Connection created")
@@ -11,7 +18,7 @@ io.on('connection', function(socket) {
   })
 })
 
-server.listen(3030, function() {
+server.listen(process.env.PORT || 3300, function() {
   const host = server.address().address
   const port = server.address().port
   console.log(`Express server is running on host ${host}:${port}`)
